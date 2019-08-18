7febd4abfc363997447c652453ba5cd75fb08aba
sam.engleang <sam.engleang@asiacell.com>
2019-08-18 23:01:59 +0300

    Init

---
+++ src/layout/dashboard/ContentFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+       <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Khmer word Segmentation
+          </a>
+        </li>
+        
+      </ul>
+      <div class="copyright">
+        demo
+      </div> 
+    </div>
+  </footer>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        year: new Date().getFullYear()
+      }
+    }
+  };
+</script>
+<style>
+</style>
